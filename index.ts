3537ca85713702183197372c20c5e2c341d69a94
mariam-movsisyan <109759845+mariam-movsisyan@users.noreply.github.com>
2022-10-31 23:27:22 +0400

    Modified version of the code

--- index.ts
+++ index.ts
@@ -1,66 +1,73 @@
-type typeIsProject = {
-    name: string;
-}
-type typeIsManager ={
+interface IEmployee {
     name: string;
     surname: string;
-    projects: typeIsProject[];
-}
-
-
-type typeIsTeamLead ={
-    name: string;
-    surname: string;
-}
-type typeIsDeveloper = {
-    name:string;
-    surname: string;
-}
-
-class Employee {
-    name: string;
-    surname: string;
-    constructor(name: string, surname: string){
-        this.name = name;
-        this.surname = surname;
-    }
 }
 enum Sphere{
     FrontEnd = 'Frontend developer',
     Backend = 'Backend developer'
 }
-class Director extends Employee {
-    private managers!: typeIsManager[];
-    public teamLeade!:typeIsTeamLead[]
+enum Profession{
+    DIRECTOR = 'Director',
+    MANAGER = 'Manager',
+    TEAMLEAD = 'TeamLead',
+    DEVELOPER = 'Developer'
+}
+class Employee implements IEmployee {
+    public name: string;
+    public surname: string;
+    public prfession :Profession
 
-    constructor(name: string, surname: string){
-        super(name, surname);
-        this.managers = [];
-    }
-    addManager(manager: typeIsManager){
-        this.managers.push(manager);
-    }
-    getAllManagers(){
-        return this.managers;
-    }
-    getAllProjects(){
-        let projectsResult = []
-        if(this.managers.length !== 0){
-            for(let i: number = 0; i < this.managers.length; i++){
-                projectsResult.push(this.managers[i].projects);
-            }
-        }
-        return projectsResult;
+    constructor (name: string, surname: string, profession: Profession){
+        this.name = name;
+        this.surname = surname;
+        this.prfession = profession;
     }
 }
+class Developer extends Employee{
+    public sphere!: string
+    constructor(name: string, surname: string,profession: Profession, sphere: Sphere ){
+        super(name, surname,profession);
+        this.sphere = sphere;
+    }
+}
+
+class TeamLead extends Employee{
+    public developers: Developer[]
+    constructor(name:string, surname: string, profession: Profession){
+        super(name, surname, profession);
+        this.developers = [];
+    }
+    addDevelodep(developer: Developer){
+        this.developers.push(developer);
+    }
+    getDevelopers(){
+        return this.developers
+    }
+}
+
+class Project {
+    public name: string;
+    public teamLeads : TeamLead[]
+    constructor(name: string){
+        this.name = name;
+        this.teamLeads = [];
+    }
+    addTeamLeads(teamLead: TeamLead){
+        this.teamLeads.push(teamLead)
+    }
+    getTeamLeads(){
+        return this.teamLeads
+    }
+}
+
 class Manager extends Employee {
-    public projects: typeIsProject[];
-    constructor(name: string, surname: string){
-        super(name, surname);
+    public projects: Project[];
+    constructor(name: string, surname: string, profession: Profession){
+        super(name, surname,profession);
         this.projects = []
     }
 
-    addNewProject(project: typeIsProject){
+    addNewProject(project: Project){
         this.projects.push(project);
     }
     getProjects(){
@@ -68,65 +75,69 @@ class Manager extends Employee {
     }
 }
 
-class Project {
-    public name!: string;
-    public teamLeadResult!: typeIsTeamLead;
-    constructor(name: string){
-        this.name = name;
+class Director extends Employee {
+    public managers!: Manager[];
+    constructor(name: string, surname: string, profession: Profession){
+        super(name, surname, profession);
+        this.managers = [];
     }
-    addTeamLeads(teamLead: typeIsTeamLead){
-        this.teamLeadResult = teamLead;
+    addManager(manager: Manager){
+        this.managers.push(manager);
     }
-    getTeamLeads(){
-        return this.teamLeadResult
+    getAllManagers(){
+        return this.managers;
     }
-}
-
-class TeamLead extends Employee{
-    public developers: typeIsDeveloper[];
-    constructor(name:string, surname: string){
-        super(name, surname);
-        this.developers = [];
-    }
-
-    addDevelopers(developer:typeIsDeveloper){
-        this.developers.push(developer);
-    }
-
-    getDevelopers(){
-        return this.developers;
-    }
-
-}
-class Developer extends Employee{
-    public sphere!: string
-    constructor(name: string, surname: string, sphere: Sphere ){
-        super(name, surname)
-        this.sphere = sphere
+    findValuesWithkey(arr: any, argKey: string) {
+        const filteredData: any[] = [];
+        for (const item of arr) {
+            for (const key in item) {
+                if (key === argKey) {
+                    filteredData.push(item[key]);
+                }
+                if (typeof item[key] === 'object') {
+                    if (Array.isArray(item[key])) {
+                        filteredData.push(...this.findValuesWithkey(item[key], argKey))
+                    } else {
+                        filteredData.push(...this.findValuesWithkey([item[key]], argKey))
+                    }
+                }
+            }
+        } 
+        return filteredData;
     }
 }
 
-
-let director = new Director('Mariam', 'Movsisyan');
-let manager = new Manager('Anna', 'Harutyunyan');
-let manager2 = new Manager ('Varduhi', 'Karapetyan');
+let developer = new Developer('Karen', 'Hayrapetyan',Profession.DEVELOPER, Sphere.FrontEnd);
+let developer2 = new Developer('Levon', 'Safaryan',Profession.DEVELOPER, Sphere.Backend);
+let teamLead = new TeamLead('Marine', 'Grigoryan',Profession.TEAMLEAD);
+let teamLead2 = new TeamLead('Vahan', 'Gasparyan', Profession.TEAMLEAD);
 let project = new Project('TypeScript Project');
 let project2 = new Project('Angular Project');
-let teamLead = new TeamLead('Marine', 'Grigoryan');
-let teamLead2 = new TeamLead('Vahan', 'Gasparyan');
-let developer = new Developer('Karen', 'Hayrapetyan', Sphere.FrontEnd);
-let developer2 = new Developer('Levon', 'Safaryan', Sphere.Backend);
+let manager = new Manager('Anna', 'Harutyunyan', Profession.MANAGER);
+let manager2 = new Manager ('Varduhi', 'Karapetyan', Profession.MANAGER);
+let director = new Director('Mariam', 'Movsisyan',Profession.DIRECTOR);
 
-director.addManager(manager)
-director.addManager(manager2)
+teamLead.addDevelodep(developer)
+teamLead2.addDevelodep(developer2);
+project.addTeamLeads(teamLead)
+project2.addTeamLeads(teamLead2)
 manager.addNewProject(project)
 manager2.addNewProject(project2);
-project.addTeamLeads(teamLead);
-project2.addTeamLeads(teamLead2); 
-teamLead.addDevelopers(developer);
-teamLead2.addDevelopers(developer2);
+director.addManager(manager)
+director.addManager(manager2)
 
-console.log(director.getAllManagers());
-console.log(director.getAllProjects());
-console.log(manager.getProjects());
-console.log(project2.getTeamLeads());
+let x = []
+x.push(director);
+console.log(director.findValuesWithkey(x, 'managers'))
+console.log(director.findValuesWithkey(x, 'projects'))
+console.log(director.findValuesWithkey(x, 'teamLeads'))
+console.log(director.findValuesWithkey(x, 'developers'))
+
+// console.log(teamLead.getDevelopers())
+// console.log(teamLead2.getDevelopers())
+// console.log(project.getTeamLeads())
+// console.log(project2.getTeamLeads())
+// console.log(manager.getProjects());
+// console.log(manager2.getProjects());
+// console.log(director.getAllManagers());
+// console.log(director.getAllProjects());
